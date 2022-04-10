0eefd41d2e2b27de4e2a1dcb6658dac8b9ace197
nurgulsezgin <nurgul_sezgin@hotmail.com>
2022-04-11 01:46:49 +0300

    IfStatements

---
+++ IfStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿//int time = DateTime.Now.Hour;
+int time = 22;
+if (time>= 5 && time<= 12) 
+{
+  Console.WriteLine("Good morning.");
+} 
+else if (time> 12 && time<= 16) 
+{
+  Console.WriteLine("Good afternoon.");
+}
+else if (time> 16 && time<= 20) 
+{
+  Console.WriteLine("Good evening.");
+} 
+else // when the sun goes down
+{
+  Console.WriteLine("Good nights.");
+}
+
+//Ternary Operator(Short Hand If...Else)
+int time2 = 10;
+string result = (time2 < 18) ? "Good day." : "Good evening.";
+Console.WriteLine(result);
+Console.Read();
