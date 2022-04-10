beb7bd0bcb982caef2081a6adbb47bd2234d9062
nurgulsezgin <nurgul_sezgin@hotmail.com>
2022-04-11 00:52:39 +0300

    ExceptionHandling

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -12,7 +12,31 @@ catch(Exception ex)//{ Hata ile karşılaşıldığında ne yapılacağı buraya
 {
     Console.WriteLine("Bir Hata Oluştu: "+ "Hata:" + ex.Message.ToString());
 }
-finally//{ Hata olsun olmasın mutlaka yapılmasını istediğimiz işler varsa buraya yazarız }
+finally//{ Hata olsun olmasın mutlaka yapılmasını istediğimiz işler varsa buraya yazarız. }
 {
     Console.WriteLine("İşlem tamamlandı.");
 }
+
+
+try
+{
+   // int a=int.Parse(null);
+    int b=int.Parse("null");
+   // int c=int.Parse("-200000000000");
+}
+catch(ArgumentNullException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Boş bir değer girdiniz");
+}
+catch  (FormatException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Veri tipi uygun degil");
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine(ex);
+    Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz");
+}
+Console.ReadLine();
