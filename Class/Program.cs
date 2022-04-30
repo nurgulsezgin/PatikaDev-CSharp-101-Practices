3de16541254bf379685587dfcd50de8b10cfbbb9
nurgulsezgin <nurgul_sezgin@hotmail.com>
2022-05-01 01:19:10 +0300

    Constructor Method

--- Class/Program.cs
+++ Class/Program.cs
@@ -15,37 +15,52 @@
 //*Internal : Sadece bulunduğu proje içerisinden erişilebilir
 //*Protected : Sadece tanımlandığı sınıfta ya da o sınıfı miras alan sınıflardan erişilebilir.
 
-Employee employee1=new Employee();
-employee1.name="Ayşe";
-employee1.lastName="Kara";
-employee1.employeeNo=1000132;
-employee1.departmant="İnsan Kaynakları";
+Employee employee1=new Employee("Ayşe","Kara",1000132,"İnsan Kaynakları");
 
 employee1.employeeInformation();
 
 Employee employee2=new Employee();
-employee2.name="Deniz";
-employee2.lastName="Arda";
-employee2.employeeNo=1000456;
-employee2.departmant="Satın Alma";
+employee2.Name="Deniz";
+employee2.LastName="Arda";
+employee2.EmployeeNo=1000456;
+employee2.Departmant="Satın Alma";
 
 employee2.employeeInformation();
+
+Employee employee3=new Employee("Zikriye","Ürkmez");
+
+employee3.employeeInformation();
 
 Console.WriteLine("\nPlease touch a key to close it..");
 Console.ReadKey();
 
 class Employee
 {
-    public string name;
-    public string lastName;
-    public int employeeNo;
-    public string departmant;
+    public string Name;
+    public string LastName;
+    public int EmployeeNo;
+    public string Departmant;
+
+    public Employee(string name, string lastName, int employeeNo, string departmant)//Constructor Method
+    {
+        this.Name=name;
+        this.LastName=lastName;
+        this.EmployeeNo=employeeNo;
+        this.Departmant=departmant;
+    }
+    public Employee(string name, string lastName)//Method Overloading
+    {
+        this.Name=name;
+        this.LastName=lastName;
+    }
+    public Employee(){}
 
     public void employeeInformation()
     {
-        Console.WriteLine("\nÇalışanın adı: {0}", name);
-        Console.WriteLine("Çalışanın soyadı: {0}", lastName);
-        Console.WriteLine("Çalışanın numarası: {0}", employeeNo);
-        Console.WriteLine("Çalışanın departmanı: {0}", departmant);
+        Console.WriteLine("\nÇalışanın adı: {0}", Name);
+        Console.WriteLine("Çalışanın soyadı: {0}", LastName);
+        Console.WriteLine("Çalışanın numarası: {0}", EmployeeNo);
+        Console.WriteLine("Çalışanın departmanı: {0}", Departmant);
     }
 }
+
