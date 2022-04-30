4fd4b49bde1ec81ec57620d48a0c902277d721f1
nurgulsezgin <nurgul_sezgin@hotmail.com>
2022-04-30 14:56:13 +0300

    Odev3 Tamamlandı

--- Homeworks/Odev3/Program.cs
+++ Homeworks/Odev3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Bir cümle giriniz..");
+string text = Console.ReadLine();
+
+findAllVowel(text);
+
+Console.WriteLine("\n\n\nPlease touch a key to close it.");
+Console.ReadKey();
+
+static void findAllVowel(string text)
+{
+    char [] letters = new char[text.Length];
+    List<char> vowels = new List<char>(){'a','e','ı','i','o','ö','u','ü','A','E','I','İ','O','Ö','U','Ü'};
+    for(int i=0;i<text.Length;i++)
+        if(vowels.Contains(text[i]))
+            Console.Write(text[i]+", ");
+    
+}
