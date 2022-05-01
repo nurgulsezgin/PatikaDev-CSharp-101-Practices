d00b6d7bcd5c377493a83412ed5afccf23b8ca16
nurgulsezgin <nurgul_sezgin@hotmail.com>
2022-05-02 00:26:03 +0300

    Class-Struct

---
+++ ClassStructure/Program.cs
@@ -0,0 +1,30 @@
+﻿Rectangle rectangle=new Rectangle();
+rectangle.LongEdge=4;
+rectangle.ShortEdge=3;
+
+Console.WriteLine("Alan: {0}",rectangle.calculateField());
+
+Rectangle_Struct rectangle_Struct;
+rectangle_Struct.LongEdge=4;
+rectangle_Struct.ShortEdge=3;
+
+Console.WriteLine("Alan: {0}",rectangle_Struct.calculateField());
+
+class Rectangle
+{
+    public int ShortEdge;
+    public int LongEdge;
+    public long calculateField()
+    {
+        return this.ShortEdge*this.LongEdge;
+    }
+}
+struct Rectangle_Struct
+{
+    public int ShortEdge;
+    public int LongEdge;
+    public long calculateField()
+    {
+        return this.ShortEdge*this.LongEdge;
+    }
+}
