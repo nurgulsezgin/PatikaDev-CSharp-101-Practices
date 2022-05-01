26b954a9d55b19b822d6c559af056d6835d29538
nurgulsezgin <nurgul_sezgin@hotmail.com>
2022-05-02 01:39:09 +0300

    Enums

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -1,6 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(Days.Monday);
+Console.WriteLine((int)Days.Monday);//Cast dönüşüm ile indexine erişebiliriz.
 
+int tempature=32;
+if(tempature<=(int)Wheather.Cold)
+{
+    Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim");
+}
+else if(tempature<=(int)Wheather.Sweltering)
+{
+    Console.WriteLine("Dışarıya çıkmak için havan aşırı sıcak");
+}
+else if(tempature>=(int)Wheather.Cool && tempature<=(int)Wheather.Hot)
+{
+    Console.WriteLine("Hadi dışarı çıkalım");
+}
 enum Days
 {
     Monday,
@@ -10,5 +24,15 @@ enum Days
     Friday,
     Saturday,
     Sunday,
+
+}
+enum Wheather
+{   
+    Freezing=-20,
+    Cold=5,
+    Cool=15,
+    Warm=20,
+    Hot=30,
+    Sweltering=40,
 
 }
