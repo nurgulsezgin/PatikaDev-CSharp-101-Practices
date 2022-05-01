037dec6c12fc3a7e1b8577f631088a1d12e900ff
nurgulsezgin <nurgul_sezgin@hotmail.com>
2022-05-02 01:04:02 +0300

    ClassStatic

---
+++ Enum/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Days.Monday);
+
+enum Days
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+
+}
