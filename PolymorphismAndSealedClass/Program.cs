acf17596687f1a996a3e26f7a1a705ae6bdfcf67
NurgulSezgin <nurgul_sezgin@hotmail.com>
2022-07-28 22:30:34 +0300

    Polymorphism

---
+++ PolymorphismAndSealedClass/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program  //Polymorphism:Çok biçimlilik
+{
+    public class Base  
+    {  
+        public virtual void Show()  
+        {  
+            Console.WriteLine("Show From Base Class.");  
+        }  
+    }  
+    public class Derived : Base  
+    {  
+        //the keyword "override" change the base class method.  
+        public override void Show()  
+        {  
+            Console.WriteLine("Show From Derived Class.");  
+        }  
+    }  
+    static void Main(string[] args)  
+    {  
+        Base objBaseRefToDerived  = new Derived();  
+        objBaseRefToDerived .Show();//Output--> Show From Derived Class.  
+        Console.ReadLine();  
+    } 
+}
