ff1acc82e653fdde545160f90627a5d00418c3c1
NurgulSezgin <nurgul_sezgin@hotmail.com>
2022-08-17 02:11:36 +0300

    Main menu

---
+++ Project2ToDoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :)");
+Console.WriteLine("*******************************************");
+Console.WriteLine("(1) Board Listelemek");
+Console.WriteLine("(2) Board'a Kart Eklemek");
+Console.WriteLine("(3) Board'dan Kart Silmek");
+Console.WriteLine("(4) Kart Taşımak");
+
+int selection = int.Parse(Console.ReadLine());
+
+switch (selection)
+{
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    default:
+        break;
+}
