32a0dae5612a90396a3ce4760bcf52e24425129a
NurgulSezgin <nurgul_sezgin@hotmail.com>
2022-08-16 13:55:54 +0300

    Operations: 1-Telefon Numarası Kaydet 2-Telefon Numarası Sil 3-Telefon Numarası Güncelle 4-Rehber Listeleme (A-Z, Z-A seçimli) 5-Rehberde Arama

---
+++ Project1PhoneBook/Project1PhoneBook/Operations/IOperation.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1PhoneBook.Operations;
+internal interface IOperation
+{
+    public void Save() { }
+    public void Delete() { }
+    public void Update() { }
+    public void List() { }
+    public void Serach() { }
+}
--- Project1PhoneBook/Project1PhoneBook/Program.cs
+++ Project1PhoneBook/Project1PhoneBook/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :) ");
+Console.WriteLine("*******************************************");
+Console.WriteLine("(1) Yeni Numara Kaydetmek");
+Console.WriteLine("(2) Varolan Numarayı Silmek");
+Console.WriteLine("(3) Varolan Numarayı Güncelleme");
+Console.WriteLine("(4) Rehberi Listelemek");
+Console.WriteLine("(5) Rehberde Arama Yapmak");
+
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        break;
+
+    case "2":
+        break;
+
+    case "3":
+        break;
+
+    case "4":
+        break;
+
+    case "5":
+        break;
+}
